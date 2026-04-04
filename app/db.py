94c2209625f2f2cfe60dd6610d6a1fad0c607866
Ivan <arias.ivan@gmail.com>
2026-04-04 19:25:46 +0200

    db con updated

--- app/db.py
+++ app/db.py
@@ -1,11 +1,12 @@
-import psycopg
-import os
+import psycopg2
 from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env
 load_dotenv()
 
+# Fetch variables
 DATABASE_URL = os.getenv("DATABASE_URL")
-print("DB URL:", DATABASE_URL)  # ← add this temporarily
 
-def get_conn():
-    return psycopg.connect(DATABASE_URL)
+# Connect to the database
+connection = psycopg2.connect(DATABASE_URL)
