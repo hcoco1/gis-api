37a30dc35432c8bc80624f40c6d716b0fab14397
Ivan <arias.ivan@gmail.com>
2026-04-04 18:49:54 +0200

    testing env

--- app/db.py
+++ app/db.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DB URL:", DATABASE_URL)  # ← add this temporarily
 
 def get_conn():
     return psycopg.connect(DATABASE_URL)
