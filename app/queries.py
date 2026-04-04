91d2fa1319a378bc62239aa9bed6068bd1b4caa9
Ivan <arias.ivan@gmail.com>
2026-04-04 21:26:27 +0200

    python string updated

--- app/queries.py
+++ app/queries.py
@@ -1,20 +1,20 @@
-
-
+# NEVER use ; inside Python SQL strings
 def fetch_geojson_bbox(conn, table, minx, miny, maxx, maxy):
     query = f"""
     SELECT json_build_object(
         'type', 'FeatureCollection',
-        'features', json_agg(
-            json_build_object(
-                'type', 'Feature',
-                'geometry', ST_AsGeoJSON(geom)::json,
-                'properties', to_jsonb(t) - 'geom'
-            )
-        )
+        'features', json_agg(feature)
     )
-    FROM {table} t
-    WHERE geom && ST_MakeEnvelope(%s, %s, %s, %s, 4326);
-    LIMIT 2000;
+    FROM (
+        SELECT json_build_object(
+            'type', 'Feature',
+            'geometry', ST_AsGeoJSON(geom)::json,
+            'properties', to_jsonb(t) - 'geom'
+        ) AS feature
+        FROM {table} t
+        WHERE geom && ST_MakeEnvelope(%s, %s, %s, %s, 4326)
+        LIMIT 2000
+    ) sub
     """
 
     with conn.cursor() as cur:
