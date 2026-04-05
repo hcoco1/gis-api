f10c5f1d0cd8a55c922e8ad800d38d82ce20094b
Ivan <arias.ivan@gmail.com>
2026-04-05 23:33:42 +0200

    shared poll increases  to10

--- app/db.py
+++ app/db.py
@@ -6,8 +6,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Create a shared pool (min 1, max 3 connections)
-_pool = psycopg2.pool.ThreadedConnectionPool(1, 3, DATABASE_URL, sslmode="require")
+# Create a shared pool (min 1, max 10 connections)
+_pool = psycopg2.pool.ThreadedConnectionPool(1, 10, DATABASE_URL, sslmode="require")
 
 def get_conn():
     return _pool.getconn()
